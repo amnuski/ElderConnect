3ec25dabb06e9a2f463b0760b89527214bef9b41
amnuski <amnuskii@gmail.com>
2025-09-13 00:50:06 +0530

    by nuski

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function HomeScreen() {
+ 
+}
+
+
