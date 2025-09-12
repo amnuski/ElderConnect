d761c860230b4362391b4b2f2aab3e21b3e02df6
amnuski <amnuskii@gmail.com>
2025-09-13 01:25:08 +0530

    home

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet,Text } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function HomeScreen() {
 
-   
+   return (
+    <Text>Home</Text>
+   )
+      
 }
 
