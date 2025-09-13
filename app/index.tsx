bcfd9f57e3dd0ec595a041c32305ec8aae9722d0
amnuski <amnuskii@gmail.com>
2025-09-13 12:00:07 +0530

    abiraj

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet,Text } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function HomeScreen() {
